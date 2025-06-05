0e6b32b3657d4d6d5283fbe2c25babbf378334fd
VERSIONpy <srivardhansirikonda02@gmail.com>
2025-06-05 10:46:54 +0530

    convert currency to USD

---
+++ currency.py
@@ -0,0 +1,10 @@
+# Write code below 💖
+a = float(input("What do you have left in pesos? "))
+a_new = a/4108.67
+b = float(input("What do you have left in soles? "))
+b_new = b/3.62
+c = float(input("What do you have left in reais? "))
+c_new = c/5.64
+
+D = a_new + b_new + c_new
+print(D)
